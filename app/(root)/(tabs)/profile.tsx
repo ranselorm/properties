2b8f3df4f2c830ead9037a6a4f3110b4ffcf2c02
ranselorm <ranselorm@outlook.com>
2025-02-21 21:38:56 +0000

    Add profile heading

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,11 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import icons from "@/constants/icons";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className="h-full flex-1 bg-white">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName="pb-4 px-7"
+      >
+        <View className="items-center justify-between flex-row mt-5">
+          <Text className="text-xl font-rubik-bold">Profile</Text>
+          <Image source={icons.bell} className="size-6" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
